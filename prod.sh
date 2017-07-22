e9566f7993e77b59b0aa6142cbc1e9dc5b9f08c3
Christophe Gragnic <cgragnic@protonmail.com>
2017-07-22 08:05:46 +0200

    Add prod.sh

---
+++ prod.sh
@@ -0,0 +1,9 @@
+git co gh-pages
+git merge master
+lein clean
+lein cljsbuild once min
+cp resources/public/js/compiled/app.js resources/public/js/compiled/prod.js
+git add resources/public/js/compiled/prod.js -f
+git commit -m "Update prod.js"
+git push origin gh-pages
+git co master
